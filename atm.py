2f2a9837b7872b6a58d2adb061fe5181c7ec2412
simranpunj18 <75869955+simranpunj18@users.noreply.github.com>
2022-01-05 20:24:00 -0600

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,17 @@
+class atm(object):
+def _init_(self,balance,PIN,statements,widrawal):
+        self.balance = balance
+        self.PIN = PIN
+        self.statements = statements
+        self.widrawal = widrawal
+
+    def statements();
+      self.statements[added] = statements
+      self.statements[subtracted] = statements
+
+    def getStatements(self,balance);
+        return self.statements[added]
+        return self.statements[subtracted]
+
+    Deposit = atm("$100,000",3490,"In the last 3 days, $3 have been added, now your balance is $100,003",1)
+    widrawal = atm("$100,002",3490,"In the last 3 days, $1 has been taken out, now your balance is $100,002",1)
